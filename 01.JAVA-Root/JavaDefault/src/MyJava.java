f1b89eedb7421d3e951a85c625e26a22d4d5b679
kosi6971 <84139325+kosi6971@users.noreply.github.com>
2022-10-24 22:33:23 +0900

    2022/10/24/4
    4

--- 01.JAVA-Root/JavaDefault/src/MyJava.java
+++ 01.JAVA-Root/JavaDefault/src/MyJava.java
@@ -1,6 +1,6 @@
 public class MyJava {
     public static void main(String[] args) {
-        System.out.println("나야나");
-        System.out.println("너두냐");
+        System.out.println("kosi");
+        System.out.println("clear");
     }
 }
